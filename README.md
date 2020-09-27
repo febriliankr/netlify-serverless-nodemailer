a76d726705f08c95e14b552dd694d8f89426be77
febriliankr <febrilian.kr@gmail.com>
2020-09-27 23:42:46 +0700

    finally working

---
+++ README.md
@@ -0,0 +1,11 @@
+# Trying out Netlify Serverless Functions with Express.js
+
+`npm start` to start the server
+
+then
+
+Send POST request to /.netlify/functions/api/mail to use the nodemailer app.
+
+## It's Working!!!
+
+27 Sept 2020, 23:38
---
+++ src/api.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const serverless = require("serverless-http");
+const nodemailer = require("nodemailer");
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi",
+  });
+});
+
+router.post("/mail", async (req, res) => {
+  console.log("req", req.body);
+  const { email } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "milford.marquardt@ethereal.email",
+      pass: "Rcer1ezEHjCZEpDV5V",
+    },
+  });
+
+  const message = {
+    from: '"Serverless Nodemailing" <febrilian.kr@gmail.com>', // sender address
+    to: `${email}`, // list of receivers
+    subject: "Serverless is ON, Baby", // Subject line
+    text: "Hello, long time no see.", // plain text body
+    html: "<b>Hello, long time no see.</b>", // html body
+  };
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail(message);
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.send(`Email sent!`);
+});
+
+module.exports.handler = serverless(app);
+
+app.use("/.netlify/functions/api", router);
